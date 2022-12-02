6959f374dbcb392dae7d47ba01f147f46812a66b
Law Patrick <plj611@outlook.com>
2022-12-02 16:00:47 +0800

    Add check logic

--- src/build_comp_db.py
+++ src/build_comp_db.py
@@ -1,17 +1,21 @@
 import pandas
 import requests
+import sys
 from bs4 import BeautifulSoup
 
-compdb = pandas.read_excel('./compDB.xlsx','1-99')
-df = pandas.DataFrame({'code':[],
-                        'description':[]})
+compdb = pandas.read_excel('./compDB.xlsx', sys.argv[1])
+
+if sys.argv[2] != 'check':
+   df = pandas.DataFrame({'code':[], 'company':[], 'description':[]})
 
 for c, n in zip(compdb['name2'].tolist(), compdb['number'].tolist()):
    url = f'https://zh.wikipedia.org/api/rest_v1/page/html/{c}'
 
    re = requests.get(url)
 
-   if re.status_code != 404:
+   print(f'Retrieving {n} - {c}')
+
+   if re.status_code != 404 and sys.argv[2] != 'check':
       soup = BeautifulSoup(re.text, features="html.parser")
 
       # kill all script and style elements
@@ -36,11 +40,16 @@ for c, n in zip(compdb['name2'].tolist(), compdb['number'].tolist()):
          except StopIteration:
             break
          #print('3', l, len(l))
-         df = pandas.concat([df, pandas.DataFrame({'code': n, 'description': l}, index=[1])], ignore_index=True)
+         df = pandas.concat([df, pandas.DataFrame({'code': n, 'company': c, 'description': l}, index=[1])], ignore_index=True)
       #print('3', l, len(l))
-      df = pandas.concat([df, pandas.DataFrame({'code': n, 'description': l}, index=[1])], ignore_index=True)
+      df = pandas.concat([df, pandas.DataFrame({'code': n, 'company': c, 'description': l}, index=[1])], ignore_index=True)
       re.close()
    else:
-      #print(c, n)
-      print(df)
-df.to_excel('./a.xlsx')
+      if sys.argv[2] == 'check':
+         if re.status_code == 404:
+            print(f'{n} - {c}')
+      else:
+         df = pandas.concat([df, pandas.DataFrame({'code': n, 'company': c, 'description': 'No information'}, index=[1])], ignore_index=True)
+
+if sys.argv[2] != 'check':
+   df.to_excel('./a.xlsx', sheet_name = sys.argv[1], index=False)
